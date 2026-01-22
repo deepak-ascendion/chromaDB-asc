2688dd8b9cff42e0691b09cbf7d34da940ec33d4
Tanuj Nayak <tanujnay112@live.com>
2026-01-22 03:42:58 -0800

    [ENH]: increment_compaction_failure_count on rust sysdb

--- rust/sysdb/src/sysdb.rs
+++ rust/sysdb/src/sysdb.rs
@@ -1315,6 +1315,7 @@ impl GrpcSysDb {
             }),
             dimension: dimension.map(|dim| dim as i32),
             configuration_json_str,
+            cursor_updates: None,
         };
 
         client.update_collection(req).await.map_err(|e| {
