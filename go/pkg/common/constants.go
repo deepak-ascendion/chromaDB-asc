15d046cfd45d98738851d7a6e5b5568f75f3530b
deepak.suresh <deepak.suresh@ascendion.com>
2026-01-28 14:28:30 +0530

    wip: chromadb vulnerabilities fixes for 0.5.20

--- go/pkg/common/constants.go
+++
@@ -1,6 +0,0 @@
-package common
-
-const (
-	DefaultTenant   = "default_tenant"
-	DefaultDatabase = "default_database"
-)
--- go/pkg/sysdb/metastore/db/migrations/20240621171854.sql
+++
@@ -1,2 +0,0 @@
--- Modify "collections" table
-ALTER TABLE "public"."collections" ADD COLUMN "configuration_json_str" text NULL;
